10d10e016f863c0a5ea910e22ade3fa2e790f278
karenartc <karenrc.rrss@gmail.com>
2026-01-14 17:47:40 -0300

    CReate function to play the game 5 times and the logic for each round and who scores the point. Modified a text in the html file

--- script.js
+++ script.js
@@ -15,11 +15,52 @@ function getHumanChoice(){
 }
 
 //Create function for the game to be played 5 times
-
+function playGame(){
     //Create two variables 'humanScore' and 'computerScore' 
+    let humanScore = 0;
+    let computerScore = 0;
+    let tieScore = 0;
 
     //Create function that accepts two parameters (humanChoice and ComputerChoice). It has to increment the score of each player for round, and show a text that says something like 'You Won! Rock beats scissors.
+    function playRound(computerChoice, humanChoice){
+        if(computerChoice == humanChoice){
+            alert(`You both played ${computerChoice}, No one score a point.`);
+            return 'tie';
+        } else if((computerChoice == 'rock') && (humanChoice == 'scissors')){
+            alert('You lost! Rock beats scissors.');
+            return 'computer';
+        } else if((computerChoice == 'paper') && (humanChoice == 'rock')){
+            alert('You lost! Paper beats rock.');
+            return 'computer';
+        } else if((computerChoice == 'scissors') && (humanChoice == 'paper')){
+            alert('You lost! Scissors beats paper.');
+            return 'computer';
+        } else if((humanChoice == 'rock') && (computerChoice == 'scissors')){
+            alert('You Won! Rock beats scissors.');
+            return 'human';
+        } else if((humanChoice == 'paper') && (computerChoice == 'rock')){
+            alert('You Won! Paper beats rock.');
+            return 'human';
+        } else if((humanChoice == 'scissors') && (computerChoice == 'paper')){
+            alert('You Won! Scissors beats paper.');
+            return 'human';
+        }
+    }
 
+    //loop for the game to be played 5 times
+    for(let i=1; i<=5; i++){
+        let winner = playRound(getComputerChoice(), getHumanChoice());
+        if (winner == 'computer') {
+            computerScore += 1;
+        }else if (winner == 'human'){
+            humanScore += 1;
+        }else if (winner == 'tie'){
+            tieScore +=1;
+        }
+    }
+
+    //last message to the user with finals scores
+    alert(`Your score: ${humanScore} vs computer score: ${computerScore}. You both tied ${tieScore} times.`);
+}
     
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+playGame();
