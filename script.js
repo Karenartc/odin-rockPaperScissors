5a727a3fb8941f3fc69f2f8167f16930f51f4626
karenartc <karenrc.rrss@gmail.com>
2026-01-14 16:42:20 -0300

    Create function that returns the choice of the computer

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //Create function that returns the choice of the computer.
+function getComputerChoice() {
+    let numOption = Math.random() * (1 - (-1)) + (-1); 
+    numOption = numOption.toFixed(0); //round to the closest integer
+    let option = numOption > 0 ? 'scissors' :
+                 numOption < 0 ? 'rock' :
+                 'paper';
+    return option;
+}
 
 //Create function that returns the choice of the user.
 
@@ -7,4 +15,6 @@
     //Create two variables 'humanScore' and 'computerScore' 
 
     //Create function that accepts two parameters (humanChoice and ComputerChoice). It has to increment the score of each player for round, and show a text that says something like 'You Won! Rock beats scissors.
+
     
+console.log(getComputerChoice());
