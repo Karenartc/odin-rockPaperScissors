be2fe98cf6b9d5212fc11a9f8a2858fcab6f5267
karenartc <karenrc.rrss@gmail.com>
2026-01-14 16:30:33 -0300

    first commit. Basic structure HTML and comments about the logic in JS

---
+++ script.js
@@ -0,0 +1,10 @@
+//Create function that returns the choice of the computer.
+
+//Create function that returns the choice of the user.
+
+//Create function for the game to be played 5 times
+
+    //Create two variables 'humanScore' and 'computerScore' 
+
+    //Create function that accepts two parameters (humanChoice and ComputerChoice). It has to increment the score of each player for round, and show a text that says something like 'You Won! Rock beats scissors.
+    
